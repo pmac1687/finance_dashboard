f1f0c59571ff34d5d499eb256035c0fa961539c3
pmac1687 <pmac1687@outlook.com>
2020-10-04 16:38:24 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './assets/styles/tailwind.css';
+import './assets/styles/index.css';
+import Dashboard from './dashboard/index';
+import Sidebar from './components/Sidebar';
+import AdminNavbar from './components/AdminNavbar';
+import HeaderStats from './components/HeaderStats';
+import FooterAdmin from './components/FooterAdmin';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-gray-200">
+        <AdminNavbar />
+        {/* Header */}
+        <HeaderStats />
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          <Dashboard />
+          <FooterAdmin />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
